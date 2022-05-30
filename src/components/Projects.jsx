7a8070d28b71ebb088eecbb579920e4c3e22a054
Code-MNA <03mna.code@gmail.com>
2022-05-30 11:54:53 -0400

    web call png

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,7 +16,7 @@ class InfoProject{
 let projectArray = [
     new InfoProject("DNA Linker","A game I made for GMTK 2020 using C# and Unity 3D game engine","projects/dnalinker.png","https://im-na.itch.io/dna-linker",""),
     new InfoProject("Django Website","Website I made using bootstrap and python django","projects/django-web.png","https://codemna.pythonanywhere.com/","https://github.com/CODE-MNA/Django-Website"),
-    new InfoProject("Webcaller","A project where I used WebRTC library to establish a web call. I used Firestore as a signaling server.","projects/express-form.png","https://webcall-mna.web.app/","https://github.com/CODE-MNA/webcall")
+    new InfoProject("Webcaller","A project where I used WebRTC library to establish a web call. I used Firestore as a signaling server.","projects/web-call.png","https://webcall-mna.web.app/","https://github.com/CODE-MNA/webcall")
 ];
 
 
