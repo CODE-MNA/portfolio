2dfb479fd4b1d24d82b8f0c18f3efae2ce585202
Code-MNA <03mna.code@gmail.com>
2022-06-25 01:30:16 -0400

    added github

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,7 @@ export default class Contact extends Component {
         return (
             <div  className="contactDiv pad">
             <h3>Links</h3>
-            <a href="https://github.com/CODE-MNA" target="_blank"><object className="githubIcon" onClick={goToGithub} type="image/svg+xml" data="github.svg" width="50" height="50" alt="Fail"></object></a>
+            <a className="githubIcon" href="https://github.com/CODE-MNA" target="_blank"><object  onClick={goToGithub} type="image/svg+xml" data="github.svg" width="50" height="50" alt="Fail"></object></a>
             
            </div>
         )
