fdf6066b86b8913b82a2d2a3af3edaf70ff85db0
Code-MNA <03mna.code@gmail.com>
2022-06-25 00:59:29 -0400

    fixed github icon and layout

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,12 +3,18 @@ import "./css/contact.css"
 
 export default class Contact extends Component {
 
+   
     render() {
+        const goToGithub = () => {
+            window.open(`https://github.com/CODE-MNA`,'_blank').focus();
+        }
         return (
             <div  className="contactDiv pad">
             <h3>Contact Me</h3>
             <p>Feel free to email me at this address</p>
             <p >  <img className="mailIcon" src="mailLogo.svg"/>  Mahmed9174@conestogac.on.ca</p> 
+            <i class="fa-brands fa-github-square"></i>
+            <object onClick={goToGithub} type="image/svg+xml" data="github.svg" width="50" height="50" alt="Fail"></object>
            </div>
         )
     }
