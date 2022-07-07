5f1be8654501cdbe649c9477a674df844fa9807d
Code-MNA <03mna.code@gmail.com>
2022-07-07 00:24:52 -0400

    added linked in

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,13 +5,14 @@ export default class Contact extends Component {
 
    
     render() {
-        const goToGithub = () => {
-            window.open(`https://github.com/CODE-MNA`,'_blank').focus();
+        const goToURL = (url) => {
+            window.open(url,'_blank').focus();
         }
         return (
             <div  className="contactDiv pad">
             <h3>Links</h3>
-            <a className="githubIcon" href="https://github.com/CODE-MNA" target="_blank"><object  onClick={goToGithub} type="image/svg+xml" data="github.svg" width="50" height="50" alt="Fail"></object></a>
+            <a className="github Icon" href="https://github.com/CODE-MNA" target="_blank"><object  onClick={goToURL("https://github.com/CODE-MNA")} type="image/svg+xml" data="github.svg" width="50" height="50" alt="Fail"></object></a>
+            <a className="linkedIn Icon" href="https://www.linkedin.com/in/noman-ahmed-99957a22b/" target="_blank"><object  onClick={goToURL("https://www.linkedin.com/in/noman-ahmed-99957a22b/" )} type="image/svg+xml" data="linkedInLogo.svg" width="50" height="50" alt="Fail"></object></a>
             
            </div>
         )
