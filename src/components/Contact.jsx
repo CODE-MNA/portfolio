a682b5433078cd44a6ee419f18b1b607c9e5db6f
Code-MNA <03mna.code@gmail.com>
2022-08-29 00:38:31 -0400

    Added Poster Project

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,8 +11,8 @@ export default class Contact extends Component {
         return (
             <div  className="contactDiv pad">
             <h3>Links</h3>
-            <a className="github Icon" href="https://github.com/CODE-MNA" target="_blank"><object  onClick={goToURL("https://github.com/CODE-MNA")} type="image/svg+xml" data="github.svg" width="50" height="50" alt="Fail"></object></a>
-            <a className="linkedIn Icon" href="https://www.linkedin.com/in/noman-ahmed-99957a22b/" target="_blank"><object  onClick={goToURL("https://www.linkedin.com/in/noman-ahmed-99957a22b/" )} type="image/svg+xml" data="linkedInLogo.svg" width="50" height="50" alt="Fail"></object></a>
+            <a className="github Icon" href="https://github.com/CODE-MNA" target="_blank"><object   type="image/svg+xml" data="github.svg" width="50" height="50" alt="Fail github"></object></a>
+            <a className="linkedIn Icon" href="https://www.linkedin.com/in/noman-ahmed-99957a22b/" target="_blank"><object  type="image/svg+xml" data="linkedInLogo.svg" width="50" height="50" alt="Fail linkedin"></object></a>
             
            </div>
         )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,7 +16,9 @@ class InfoProject{
 let projectArray = [
     new InfoProject("DNA Linker","A game I made for GMTK 2020 using C# and Unity 3D game engine","projects/dnalinker.png","https://im-na.itch.io/dna-linker",""),
     new InfoProject("Book Finder","A project I made using React functional components and Google books api for searching books","projects/bookfinder.png","https://code-mna.github.io/bookfinder/","https://github.com/CODE-MNA/bookfinder"),
-    new InfoProject("Webcaller","A project where I used WebRTC library to establish a web call. I used Firestore as a signaling server.","projects/web-call.png","https://webcall-mna.web.app/","https://github.com/CODE-MNA/webcall")
+    new InfoProject("Webcaller","A project where I used WebRTC library to establish a web call. I used Firestore as a signaling server.","projects/web-call.png","https://webcall-mna.web.app/","https://github.com/CODE-MNA/webcall"),
+    new InfoProject("Poster","A Fullstack website with front-end made in React connecting to a REST API programmed using node js (express), backend hosted on Heroku.","projects/poster.png","https://code-mna.github.io/Poster-FrontEnd/","https://github.com/CODE-MNA/Poster-BackEnd")
+
 ];
 
 
